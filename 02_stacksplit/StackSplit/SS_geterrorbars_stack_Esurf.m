8a943d1f4c246f90960e055e5bc0ad95fdd9223d
yvonnefroelich <yvonne.froehlich@kit.edu>
2025-02-11 09:20:31 +0100

    Add location

--- 02_stacksplit/StackSplit/SS_geterrorbars_stack_Esurf.m
+++ 02_stacksplit/StackSplit/SS_geterrorbars_stack_Esurf.m
@@ -22,7 +22,7 @@ function [errbar_phi, errbar_t, Ecrit] = SS_geterrorbars_stack_Esurf( ...
 % Copyright (C) 2025  Michael Grund & Yvonne Fröhlich (up on v4.0)
 % GitHub: https://github.com/yvonnefroehlich/splitlab_stacksplit
 % Copyright (C) 2021  Michael Grund & Yvonne Fröhlich (v3.0)
-% Copyright (C) 2016  Michael Grund (v1.0-v2.0)
+% Copyright (C) 2016  Michael Grund, Karlsruhe Institute of Technology (KIT) (v1.0-v2.0)
 % GitHub: https://github.com/michaelgrund
 %
 % This program is free software: you can redistribute it and/or modify
