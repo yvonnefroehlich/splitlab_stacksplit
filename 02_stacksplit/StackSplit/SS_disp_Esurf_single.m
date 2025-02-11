7179e1f71424478704eb0bb0eac71bfc0b815cd2
yvonnefroelich <yvonne.froehlich@kit.edu>
2025-02-11 09:25:15 +0100

    Mention fodler

--- 02_stacksplit/StackSplit/SS_disp_Esurf_single.m
+++ 02_stacksplit/StackSplit/SS_disp_Esurf_single.m
@@ -15,7 +15,7 @@ function h = SS_disp_Esurf_single(h, index)
 % LICENSE
 %
 % Copyright (C) 2025  Michael Grund & Yvonne Fröhlich (up on v4.0)
-% GitHub: https://github.com/yvonnefroehlich/splitlab_stacksplit
+% GitHub: https://github.com/yvonnefroehlich/splitlab_stacksplit -> folder 02_stacksplit
 % Copyright (C) 2021  Michael Grund & Yvonne Fröhlich (v3.0)
 % Copyright (C) 2016  Michael Grund, Karlsruhe Institute of Technology (KIT) (v1.0-v2.0)
 % GitHub: https://github.com/michaelgrund
--- 02_stacksplit/StackSplit/SS_splitdiagnosticLayout.m
+++ 02_stacksplit/StackSplit/SS_splitdiagnosticLayout.m
@@ -16,7 +16,7 @@ function [axH, axRC, axSC, axSeis, axwm] = SS_splitdiagnosticLayout(Synfig)
 % LICENSE
 %
 % Copyright (C) 2025  Michael Grund & Yvonne Fröhlich (up on v4.0)
-% GitHub: https://github.com/yvonnefroehlich/splitlab_stacksplit
+% GitHub: https://github.com/yvonnefroehlich/splitlab_stacksplit -> folder 02_stacksplit
 % Copyright (C) 2021  Michael Grund & Yvonne Fröhlich (v3.0)
 % Copyright (C) 2016  Michael Grund, Karlsruhe Institute of Technology (KIT) (v1.0-v2.0)
 % GitHub: https://github.com/michaelgrund
